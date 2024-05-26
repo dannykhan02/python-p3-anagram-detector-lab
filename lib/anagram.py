b46dd739387ccbecf215804d268b2b856015e51b
dannykhan02 <dannykhan614@gmail.com>
2024-05-26 13:17:29 +0300

    create my fil

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,22 @@
-# your code goes here!
+
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        
+        sorted_word = sorted(self.word)
+        
+       
+        matches = []
+
+        
+        for word in words:
+            
+            if sorted(word) == sorted_word:
+                
+                matches.append(word)
+        
+        
+        return matches
